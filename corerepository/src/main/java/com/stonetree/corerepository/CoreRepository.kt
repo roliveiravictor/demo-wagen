7dc381e4d1421b23288859ef5a63e6ac7b63ac9c
Victor R <victor.rocha@mercadolivre.com>
2019-07-17 23:33:42 -0300

    downgraded interceptor version preventing connection with sync repository instance

--- corerepository/src/main/java/com/stonetree/corerepository/CoreRepository.kt
+++ corerepository/src/main/java/com/stonetree/corerepository/CoreRepository.kt
@@ -1,32 +1,43 @@
 package stonetree.com.meals.core.provider
 
 import com.stonetree.corerepository.CoreRepositoryConstant
+import com.stonetree.corerepository.CoreRepositoryConstant.TIMEOUT
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class CoreRepository {
 
     lateinit var retrofit: Retrofit
 
-    init {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        OkHttpClient.Builder().addInterceptor(interceptor).build()
-    }
-
     companion object {
-        @Volatile private var instance: CoreRepository? = null
+
+        private var instance: CoreRepository? = null
         fun getInstance() =
-            instance ?: synchronized(this) {
-                instance ?: CoreRepository().also {
+            instance ?: CoreRepository().also {
                     instance = it
-                    instance?.retrofit = Retrofit.Builder()
-                        .baseUrl(CoreRepositoryConstant.BASE_URL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                }
+                    startRetrofit()
             }
+
+        private fun startRetrofit() {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val httpClient = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+                .build()
+
+            instance?.retrofit = Retrofit.Builder()
+                .baseUrl(CoreRepositoryConstant.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build()
+        }
     }
 }
+
