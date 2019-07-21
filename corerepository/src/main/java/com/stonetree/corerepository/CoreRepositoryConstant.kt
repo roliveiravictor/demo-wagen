d77e80553122a527f1643532a18e804c6e474017
Victor R <victor.rocha@mercadolivre.com>
2019-07-21 17:33:22 -0300

    removed security from base url and increased fetch positions

--- corerepository/src/main/java/com/stonetree/corerepository/CoreRepositoryConstant.kt
+++ corerepository/src/main/java/com/stonetree/corerepository/CoreRepositoryConstant.kt
@@ -2,10 +2,10 @@ package com.stonetree.corerepository
 
 object CoreRepositoryConstant {
 
-    const val BASE_URL: String = "https://api-aws-eu-qa-1.auto1-test.com/"
+    const val BASE_URL: String = "http://api-aws-eu-qa-1.auto1-test.com/"
     const val WA_KEY: String = "wa_key"
     const val WA_VALUE: String = "coding-puzzle-client-449cc9d"
     const val TIMEOUT: Long = 5
     const val PAGE_SIZE: Int = 15
-    const val FETCH_DISTANCE: Int = 1
+    const val FETCH_DISTANCE: Int = 10
 }
